d2aa97112a210601483623c7e7eee763988da139
Dan Yarmak <dyarmak@h2safety.ca>
2019-04-11 18:39:48 -0600

    It now properly amalgamates the queries. Also applied the missing yellow cell formatting

--- joinData.py
+++ joinData.py
@@ -1,5 +1,7 @@
 import openpyxl
+from openpyxl.styles import Alignment, PatternFill
 from myxlutils import format_date_rows, get_column_names_and_index
+
 # from ForecastManipulations import forecastDict
 # from InvoicedManipulations import invoicedDict
 # from CreditsManipulations import creditsDict
@@ -21,25 +23,16 @@ sInvo = wbInvo.active
 wbCred = openpyxl.load_workbook(credit)
 sCred = wbCred.active
 
-# We will use the column names from Forecast... is this right???
-rowCount = 1
-for r in range(rowCount, sFore.max_row+1):
+# -------------------- Copy Forecast -> Combined ------------------------------------- 
+foreRowCount = 0
+
+print("Looping sFore Rows")
+# We include the column names from Forecast.xlsx, not for other 2 sheets
+for r in range(1, sFore.max_row+1):
         for c in range(1, sFore.max_column+1):
                 sCombined.cell(row=r,column=c).value = sFore.cell(row=r, column=c).value
-        rowCount+=1
-for r in range(rowCount, sInvo.max_row+1):
-        for c in range(1, sInvo.max_column+1):
-                sCombined.cell(row=r,column=c).value = sInvo.cell(row=r, column=c).value
-        rowCount+=1
+        foreRowCount+=1
 
-# NOT WORKING!
-
-for r in range(rowCount, sCred.max_row+1):
-        for c in range(1, sCred.max_column+1):
-                sCombined.cell(row=r,column=c).value = sCred.cell(row=r, column=c).value
-        rowCount+=1
-
-print("Total Rows: " + str(rowCount))
 wbCombined.save("Combined.xlsx")
 wbCombined.close()
 wbCombined = openpyxl.load_workbook("Combined.xlsx")
@@ -49,5 +42,52 @@ combinedDict = {}
 get_column_names_and_index(sCombined,combinedDict)
 format_date_rows(sCombined, combinedDict, "mm-dd-yy", "Due Date", "InvoiceDateSent", "OriginalDueDate")
 
+yellowFill = PatternFill(patternType="solid", fgColor="FFFF00" )
+
+for r in range(2, sCombined.max_row+1):
+        # IF InvoiceDateSent != None, Forecast GOT None But formatting doesn't copy over. 
+        # We NOW can fill cell background with yellow
+        # HAVE to do this step AFTER copying sFore, and BEFORE copying anything else...
+        if(sCombined.cell(row=r, column=combinedDict["InvoiceDateSent"]).value != None): # May need to test for == 2018 or == 2019 instead
+                sCombined.cell(row=r, column=combinedDict["Forecast"]).fill = yellowFill 
+
+# We want to start
+combinedStartPoint = foreRowCount -1
+invoRowCount = 0        
+for r in range(2, sInvo.max_row+1):
+        for c in range(1, sInvo.max_column+1):
+                sCombined.cell(row=r+combinedStartPoint,column=c).value = sInvo.cell(row=r, column=c).value
+        invoRowCount+=1
+        
+
+wbCombined.save("Combined.xlsx")
+combinedStartPoint = foreRowCount + invoRowCount -1
+credRowCount = 0
+for r in range(2, sCred.max_row+1):
+        for c in range(1, sCred.max_column+1):
+                sCombined.cell(row=r+combinedStartPoint,column=c).value = sCred.cell(row=r, column=c).value
+        credRowCount+=1
+
+wbCombined.save("Combined.xlsx")
+
+totalRows = foreRowCount + invoRowCount + credRowCount
+print("Total Rows: " + str(totalRows))
+
+# Save and re-load workbook
+wbCombined.save("Combined.xlsx")
+wbCombined.close()
+wbCombined = openpyxl.load_workbook("Combined.xlsx")
+sCombined = wbCombined.active
+# Re-load column names into a new dict
+
+
+# ----------------- Formatting -------------------------
+sCombined.freeze_panes = "A2"
+
+alignment = Alignment(horizontal="left", vertical="top")
+for x in range(1,(sCombined.max_column+1)):
+    sCombined.cell(row=1, column=x).alignment = alignment
+
+# Save and close
 wbCombined.save("Combined.xlsx")
 wbCombined.close()
